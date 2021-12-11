b4742d0274a339bb84df21e4dd73e37e4c317ffe
Bence Balogh <benceeok@gmail.com>
2021-12-11 21:11:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="dewloosh",                     
-    version="0.0.1",                        
+    version="0.0.4",                        
     author="dewloosh",
     author_email = 'dewloosh@gmail.com',
     url = 'https://github.com/dewloosh/dewloosh',   
-    download_url = 'https://github.com/dewloosh/dewloosh/archive/refs/tags/0_0_1.zip',                     
+    download_url = 'https://github.com/dewloosh/dewloosh/archive/refs/tags/0_0_3.zip',                     
     description="A collection of tools for common developer utility",
     long_description=long_description,   
     long_description_content_type="text/markdown",
@@ -21,12 +24,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
 		'Operating System :: OS Independent'
     ],                                      
-    python_requires='>=3.6',                
-    #py_modules=["dewloosh"],             
+    python_requires='>=3.6',                            
     package_dir={'':'src'},     
-    install_requires=[            
-          'setuptools',
-          'wheel',
-      ],
+    install_requires=required,
 )
 
